ebcca21a843fa5285f3b7eaee189bb89660df195
ayacai115 <countrylife.uk@gmail.com>
2021-01-19 14:09:43 +0900

    ローカルDB構築をmakeコマンド化

---
+++ bootstrap/create_local_db.rb
@@ -0,0 +1,63 @@
+require 'aws-sdk-dynamodb'
+require 'pry'
+
+def run
+  Aws.config.update(
+    endpoint: 'http://localhost:8000',
+    region: 'ap-northeast-1'
+  )
+
+  create_table_result = create_table(dynamodb_client, table_definition)
+
+  if create_table_result == 'Error'
+    puts 'Table not created.'
+  else
+    puts "Table created with status '#{create_table_result}'."
+  end
+end
+
+private
+
+def create_table(dynamodb_client, table_definition)
+  response = dynamodb_client.create_table(table_definition)
+  response.table_description.table_status
+rescue StandardError => e
+  puts "Error creating table: #{e.message}"
+  'Error'
+end
+
+def dynamodb_client
+  Aws::DynamoDB::Client.new
+end
+
+def table_definition
+  {
+    table_name: 'musashino-kosodate-events-local',
+    key_schema: [
+      {
+        attribute_name: 'year_month',
+        key_type: 'HASH'  # Partition key.
+      },
+      {
+        attribute_name: 'date_and_id',
+        key_type: 'RANGE' # Sort key.
+      }
+    ],
+    attribute_definitions: [
+      {
+        attribute_name: 'year_month',
+        attribute_type: 'N'
+      },
+      {
+        attribute_name: 'date_and_id',
+        attribute_type: 'N'
+      }
+    ],
+    provisioned_throughput: {
+      read_capacity_units: 1,
+      write_capacity_units: 1
+    }
+  }
+end
+
+run
