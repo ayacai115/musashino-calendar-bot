311e61f8c614df00e7a9974cea339ecf5768484f
ayacai115 <countrylife.uk@gmail.com>
2021-02-10 13:29:24 +0900

    1ヶ月の予定をメッセージ用に組み立てた

--- app/lambda/notifier_function.rb
+++ app/lambda/notifier_function.rb
@@ -1,6 +1,9 @@
 require 'json'
 require 'line/bot'
 require_relative '../models/kosodate_event.rb'
+require 'pry'
+
+DAYS_OF_THE_WEEK = %w(日 月 火 水 木 金 土)
 
 def client
   @client ||= Line::Bot::Client.new { |config|
@@ -13,15 +16,28 @@ end
 def run(event:, context:)
   result = KosodateEvent.where(year: '2021', month: '02')
   result.item['data']
-  shedule = JSON.parse(result.item['data'])
+
+  schedule = JSON.parse(result.item['data'])
+
+  schedule = schedule.map do |date, events|
+    events.map! { |event| "\n#{event['name']} 【事前申込必要】\n #{event['url']}" }
+
+    date = Date.parse(date)
+    day_of_the_week = DAYS_OF_THE_WEEK[date.strftime('%w').to_i]
+    display_date = date.strftime('%-m月%-d日') + '(' + day_of_the_week + ')' # 例 2月1日(月)
+
+    [display_date, events].flatten!.join('')
+  end
+
+  schedule = schedule.join('\n---------------------------\n')
 
   message = {
     type: 'text',
-    text: 'hello'
+    text: schedule
   }
   client.broadcast(message)
 
   { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
 
-# run(event: nil, context: nil) # テスト用
+run(event: nil, context: nil) # テスト用
