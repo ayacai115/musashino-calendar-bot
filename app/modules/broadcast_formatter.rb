ae5fe10c633773c627b2be691101e8457416a625
ayacai115 <countrylife.uk@gmail.com>
2021-02-11 15:15:39 +0900

    事前申込必要をフラグで管理

--- app/modules/broadcast_formatter.rb
+++ app/modules/broadcast_formatter.rb
@@ -1,11 +1,15 @@
 class BroadcastFormatter
   DAYS_OF_THE_WEEK = %w(日 月 火 水 木 金 土)
-  DEVIDING_LINE = "\n\n--------------------------------------\n\n"
+  DEVIDING_LINE = "\n--------------------------------------\n\n"
+  BOOKING_REQUIRED = '【事前申込必要】'
 
   class << self
     def format(schedule)
       schedule = schedule.map do |date, events|
-        events.map! { |event| "\n#{event['name']} 【事前申込必要】#{event['url']}\n" }
+
+        events.map! do |event|
+          "\n#{event['name']} #{BOOKING_REQUIRED if event['booking_required']} \n#{event['url']}\n" 
+        end
         
         date = Date.parse(date)
         day_of_the_week = DAYS_OF_THE_WEEK[date.strftime('%w').to_i]
@@ -14,6 +18,12 @@ class BroadcastFormatter
         [display_date, events].flatten!.join('')
       end
 
+      formatted_message(schedule)
+    end
+    
+    private
+
+    def formatted_message(schedule)
       [{
         type: 'text',
         text: schedule[0..9].join(DEVIDING_LINE)
@@ -24,6 +34,6 @@ class BroadcastFormatter
         type: 'text',
         text: schedule[20..30].join(DEVIDING_LINE)
       }]
-    end 
+    end
   end
 end
