5953b66f0b335627226ea333941d71d76f26f153
ayacai115 <countrylife.uk@gmail.com>
2021-02-15 17:31:28 +0900

    KosodateEventのテストを作成

--- test/models/kosodate_event_test.rb
+++ test/models/kosodate_event_test.rb
@@ -2,7 +2,25 @@ require_relative '../test_helper.rb'
 require_relative '../../app/models/kosodate_event.rb'
 
 class KosodateEventTest < Minitest::Test
-  def test_sample
-    assert KosodateEvent
+  def test_initialize
+    date = Date.new(2021, 2, 1)
+    name = '親子ひろば'
+    place = '西部コミセン'
+    url = 'example.com'
+    booking_required = true
+
+    event = KosodateEvent.new(
+      date: date,
+      name: name,
+      place: place,
+      url: url,
+      booking_required: booking_required
+    )
+
+    assert_equal date, event.date
+    assert_equal name, event.name
+    assert_equal place, event.place
+    assert_equal url, event.url
+    assert_equal booking_required, event.booking_required
   end
 end
