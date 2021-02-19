94631c43b8aba0a9a796463d14ca365a0b3e4e46
ayacai115 <countrylife.uk@gmail.com>
2021-02-19 10:10:21 +0900

    dynamodbのテスト雛形

---
+++ spec/modules/dynamodb_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../app/modules/dynamodb.rb'
+
+RSpec.describe DynamoDB do
+  describe ".put" do
+    example "更新" do
+    end
+  end
+
+  describe ".get" do
+    example "参照" do
+    end
+  end
+end
