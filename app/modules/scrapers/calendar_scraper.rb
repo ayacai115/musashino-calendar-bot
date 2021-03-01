c6fcf7ecbe4bf3e3b4056f795b9da9d292e2f37d
ayacai115 <countrylife.uk@gmail.com>
2021-03-01 16:27:05 +0900

    リファクタ

--- app/modules/scrapers/calendar_scraper.rb
+++ app/modules/scrapers/calendar_scraper.rb
@@ -10,30 +10,8 @@ class CalendarScraper
     def run(next_month: false)
       calendar_table = next_month ? next_month_calendar : current_month_calendar
 
-      year_month = calendar_table.xpath("caption").text.scan(/\d+/).join("-") # 例：2021-1
-      rows = calendar_table.xpath("tr")[1..] # 1行目はヘッダなので除く
-
-      # 1列に複数日付が入っている場合があるので、まずは [日付, イベント名]の組を作る
-      events = rows.map do |row|
-        date_element = row.xpath("th").text.scan(/\d+/)[0]
-        date = Date.parse("#{year_month}-#{date_element}") 
-        
-        event_list = row.xpath("td/ul/li")
-        event_list.map { |event| [date, event]}
-      end
-
-      # before [[[<#Date>, <#Element>], [<#Date>, <#Element>]], [[<#Date>, <#Element>]]]
-      # after  [[<#Date>, <#Element>], [<#Date>, <#Element>], [<#Date>, <#Element>]]
-      events.flatten!(1) 
-
-      events.map! do |event|
-        KosodateEvent.new(
-          date: event[0],
-          name: event[1].xpath("a").text,
-          url: "http://www.city.musashino.lg.jp" + event[1].xpath("a").attribute("href").value,
-          booking_required: event[1].text.include?("事前申込必要")
-      )
-      end
+      events = scrape(calendar_table)
+      events = convert_to_kosodate_event(events)
 
       KosodateEvent.bulk_insert(events)
     end
@@ -62,6 +40,35 @@ class CalendarScraper
       document.search('br').each { |n| n.replace("\n") }
 
       document
+    end
+    
+    def scrape(calendar_table)
+      year_month = calendar_table.xpath("caption").text.scan(/\d+/).join("-") # 例：2021-1
+      rows = calendar_table.xpath("tr")[1..] # 1行目はヘッダなので除く
+
+      # 1列に複数日付が入っている場合があるので、まずは [日付, イベント名]の組を作る
+      events = rows.map do |row|
+        date_element = row.xpath("th").text.scan(/\d+/)[0]
+        date = Date.parse("#{year_month}-#{date_element}") 
+        
+        event_list = row.xpath("td/ul/li")
+        event_list.map { |event| [date, event]}
+      end
+
+      # before [[[<#Date>, <#Element>], [<#Date>, <#Element>]], [[<#Date>, <#Element>]]]
+      # after  [[<#Date>, <#Element>], [<#Date>, <#Element>], [<#Date>, <#Element>]]
+      events.flatten!(1)
+    end
+
+    def convert_to_kosodate_event(events)
+      events.map! do |event|
+        KosodateEvent.new(
+          date: event[0],
+          name: event[1].xpath("a").text,
+          url: "http://www.city.musashino.lg.jp" + event[1].xpath("a").attribute("href").value,
+          booking_required: event[1].text.include?("事前申込必要")
+      )
+      end
     end
   end
 end
