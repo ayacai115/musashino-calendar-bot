81c5ddef38422f54357efa9188a135b205339df5
ayacai115 <countrylife.uk@gmail.com>
2021-03-01 18:49:18 +0900

    統合用のクラスを作成

---
+++ app/modules/kosodate_event_integrator.rb
@@ -0,0 +1,11 @@
+class KosodateEventIntegrator
+  class << self
+    def run
+      # CalendarScraper.run
+      # OyakoHirobaScraper.run
+      # 重複したら場所だけ追加
+
+      # KosodateEvent.bulk_insert
+    end
+  end
+end
---
+++ spec/modules/kosodate_event_integrator_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../../app/modules/kosodate_event_integrator.rb'
+
+RSpec.describe KosodateEventIntegrator do
+  describe ".run" do
+  end
+end
+
