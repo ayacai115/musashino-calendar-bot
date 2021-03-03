676c1fe0ae7f9bcdde3f32bc2782f0d220e6a0de
ayacai115 <countrylife.uk@gmail.com>
2021-03-03 15:19:49 +0900

    Lambda関数からスクレイパーを呼び出す

--- app/modules/kosodate_event_integrator.rb
+++ app/modules/kosodate_event_integrator.rb
@@ -1,8 +1,11 @@
+require_relative "scrapers/calendar_scraper.rb"
+require_relative "scrapers/oyako_hiroba_scraper.rb"
+
 class KosodateEventIntegrator
   class << self
-    def run
-      calendar = CalendarScraper.run
-      oyako_hiroba = OyakoHirobaScraper.run
+    def run(next_month: false)
+      calendar = CalendarScraper.run(next_month: next_month)
+      oyako_hiroba = OyakoHirobaScraper.run(next_month: next_month)
 
       events = if calendar_includes_oyako_hiroba?(calendar)
                   add_place(calendar, oyako_hiroba)
--- spec/lambda/scraper_function_spec.rb
+++ spec/lambda/scraper_function_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../../app/lambda/scraper_function.rb'
 
 RSpec.describe 'Lambda scraper_function' do
-  describe "run" do
-    it "foo" do
+  describe ".run" do
+    example "とりあえず例外が出ないことの確認" do
+      run(event: nil, context: nil)
     end
   end
 end
