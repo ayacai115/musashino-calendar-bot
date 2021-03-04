95b8e902f955aae9d20910890acc264059e59852
ayacai115 <countrylife.uk@gmail.com>
2021-03-04 13:44:20 +0900

    notifier_functionの修正とテスト作成

--- app/modules/broadcast_formatter.rb
+++ app/modules/broadcast_formatter.rb
@@ -12,7 +12,6 @@ class BroadcastFormatter
           "\n#{event.name} #{BOOKING_REQUIRED if event.booking_required} \n#{event.url}\n" 
         end
         
-        date = Date.parse(date)
         day_of_the_week = DAYS_OF_THE_WEEK[date.strftime('%w').to_i]
         display_date = date.strftime('%-m月%-d日') + '(' + day_of_the_week + ')' # 例 2月1日(月)
 
