f59c5eab8bfa9052c6851f17adb8567a59bb2f12
ayacai115 <countrylife.uk@gmail.com>
2021-03-08 17:30:27 +0900

    publicに

--- app/modules/reply_formatter.rb
+++ app/modules/reply_formatter.rb
@@ -3,9 +3,11 @@ module ReplyFormatter
     "今月" => Date.today.month,
     "来月" => Date.today.next_month.month
   }
-  def format(text)
-    # 検索
-    # 日付、場所名を探して
-    
+
+  class << self
+    def format(text)
+      # 検索
+      # 日付、場所名を探して
+    end
   end
 end
