3838d154c59574b819453ad592a42c5b28656cfe
ayacai115 <countrylife.uk@gmail.com>
2021-03-08 15:59:51 +0900

    テストの雛形だけ作成

---
+++ spec/modules/reply_formatter_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../../app/modules/reply_formatter.rb'
+
+RSpec.describe ReplyFormatter do
+  describe ".format" do
+    example "期間で検索" do
+    end
+
+    example "イベント名で検索" do
+    end
+
+    example "場所名で検索" do
+    end
+  end
+end
