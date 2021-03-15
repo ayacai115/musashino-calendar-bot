71b3e6541758a09a77a6cf50afced3c9e403b378
ayacai115 <countrylife.uk@gmail.com>
2021-03-15 16:49:45 +0900

    例外時はステータスコード500を返す

--- app/modules/reply_formatter.rb
+++ app/modules/reply_formatter.rb
@@ -1,8 +1,8 @@
 require 'date'
 require_relative "message_formatter.rb"
+require_relative "../models/kosodate_event.rb"
 
 module ReplyFormatter
-  NO_RESULTS_MESSAGE = "まだ「今月」「来月」にしか反応できません。名前や場所検索は今後追加予定です！乞うご期待！"
 
   class << self
     def format(text)
@@ -26,7 +26,24 @@ module ReplyFormatter
     def no_result_found
       [{
         type: "text",
-        text: NO_RESULTS_MESSAGE
+        text: "まだ「今月」「来月」にしか反応できません$\n名前や場所での検索を今後追加予定です！ご期待ください$$",
+        emojis: [
+          {
+            index: 20,
+            productId: "5ac1bfd5040ab15980c9b435",
+            emojiId: "179"
+          },
+          {
+            index: 48,
+            productId: "5ac1bfd5040ab15980c9b435",
+            emojiId: "156"
+          },
+          {
+            index: 49,
+            productId: "5ac1de17040ab15980c9b438",
+            emojiId: "018 "
+          }
+        ]
       }]
     end
   end
